c1772f6df84552207e7b38b1de909295fe38ef9d
Kamil Pawelkiewicz <kamil@pawelkiewicz.pl>
2012-10-10 23:16:14 +0200

    simpler example

--- example.php
+++ example.php
@@ -2,25 +2,15 @@
 	require('profiler.php');
 
 	$profiler = new profiler;
-	declare(ticks = 30000);
+	declare(ticks = 1);
 
 	$a = array();
-	for ($i=0; $i < 100000; $i++) { 
-		switch (rand(0, 1)) {
-			case 0:
-				for ($j=0; $j < rand(0, 10000); $j++) { 
-					$a[] = $i;
-				}
-				break;
-			
-			case 1:
-				$d = rand(0, count($a));
-				for ($j=0; $j < $d; $j++) { 
-				 	if (count($a) == 0) break;
-				 	array_pop($a);
-				 } 
-				break;
-		}
+	for ($i = 0; $i < 200; $i++) { 
+		array_push($a, null);
+	}
+
+	while (!empty($a)) {
+		array_pop($a);
 	}
 
 	unset($a);
